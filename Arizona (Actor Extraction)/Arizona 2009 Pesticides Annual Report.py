145bc924bde5255e55f88218590bbfc6b73c5f20
larger-allison <45430904+larger-allison@users.noreply.github.com>
2019-04-10 16:58:54 -0400

    Update Arizona 2009 Pesticides Annual Report.py

--- Arizona (Actor Extraction)/Arizona 2009 Pesticides Annual Report.py
+++ Arizona (Actor Extraction)/Arizona 2009 Pesticides Annual Report.py
@@ -13,38 +13,93 @@ import pandas as pd
 
 chemName = []
 casN = []
+prodID = []
+templateName = []
+msdsDate = []
+recUse = []
+catCode = []
+descrip = []
+code = []
+sourceType = []
 
 tables = camelot.read_pdf(r'L:\Lab\HEM\ALarger\Actor Automated Extraction\Arizona\2009 Pesticides Annual Report (A.R.S 49-303.C)\document_320438.pdf',pages='6-39', flavor='lattice')
 i=0 
 for table in tables:
     df = tables[i].df
     df=df.drop(df.index[0])
-    if i <= 1:
+    if i <= 1: #Table 2
         df=df.drop(df.index[0])
         chemName.extend(df.loc[:,1])
         casN.extend(['']*len(df))
-    elif i <= 5:
+        prodID.extend(['1372090']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 2.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
+    elif i <= 5: #Table 3
         chemName.extend(df.loc[:,2])
         casN.extend(df.loc[:,1])
-    elif i <= 18:
+        prodID.extend(['1372091']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 3.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
+    elif i <= 18: #Table 4
         df=df.drop(df.index[0])
         chemName.extend(df.loc[:,5])
         casN.extend(df.loc[:,4])
-    elif i <= 19:
+        prodID.extend(['1372092']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 4.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
+    elif i <= 19: #Table 5
         df=df.drop(df.index[0])
         chemName.extend(df.loc[:,0])
         casN.extend(['']*len(df))
-    elif i <= 23:
+        prodID.extend(['1372093']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 5.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
+    elif i <= 23: #Table 6
         df=df.drop(df.index[0])
         chemName.extend(df.loc[:,5])
         casN.extend(df.loc[:,6])
-    else:
+        prodID.extend(['1372094']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 6.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
+    else: #Table 7
         df=df.drop(df.index[0])
         chemName.extend(df.loc[:,6])
         casN.extend(df.loc[:,7])
+        prodID.extend(['1372095']*len(df))
+        templateName.extend(['Arizona 2009 Pesticide Report Table 7.pdf']*len(df))
+        msdsDate.extend([2009]*len(df))
+        recUse.extend(['']*len(df))
+        catCode.extend(['']*len(df))
+        descrip.extend(['']*len(df))
+        code.extend(['']*len(df))
+        sourceType.extend(['ACToR Assays and Lists']*len(df))
     i+=1
 
-chemList = [] #list of chem names with cas numbers (so duplicates without cas can be deleted)
 j = len(chemName) - 1
 while j >= 0: #go through list backwards, so it doesnt mess up the index if a row is deleted
     if ',' in chemName[j] or '\n' in chemName[j]:
@@ -57,26 +112,15 @@ while j >= 0: #go through list backwards, so it doesnt mess up the index if a ro
     if chemName[j] == '':
         del chemName[j]
         del casN[j]
-    if chemName[j] not in chemList and casN[j] != '':
-        chemList.append(chemName[j])
+        del prodID[j]
+        del templateName[j]
+        del msdsDate[j]
+        del recUse[j]
+        del catCode[j]
+        del descrip[j]
+        del code[j]
+        del sourceType[j]
     j-=1
-        
-k = len(chemName) - 1
-while k >= 0:
-    if chemName[k] in chemList and casN[k] == '':
-        del chemName[k]
-        del casN[k]
-    k-=1
-    
-nIngredients = len(chemName)
-prodID = [320438]*nIngredients
-templateName = ['Arizona_PestUse_2008.pdf']*nIngredients
-msdsDate = [2009]*nIngredients
-recUse = ['Pesticides: Active Ingredients']*nIngredients
-catCode = ['ACToR_Assays_***']*nIngredients
-descrip = ['pesticide active_ingredient']*nIngredients
-code = ['Arizona_PestUse_2008_AID_1']*nIngredients
-sourceType = ['ACToR Assays and Lists']*nIngredients
   
 df = pd.DataFrame({'data_document_id':prodID, 'data_document_filename':templateName, 'doc_date':msdsDate, 'raw_category':recUse, 'raw_cas':casN, 'raw_chem_name':chemName, 'cat_code':catCode, 'description_cpcat': descrip, 'cpcat_code':code, 'cpcat_sourcetype':sourceType})
 df=df.drop_duplicates()
