29446cda7eb5dd0fdb6682312bfa3387be2ef623
larger-allison <45430904+larger-allison@users.noreply.github.com>
2019-04-10 12:46:54 -0400

    Update Arizona 2001 Pesticides Annual Report.py

--- Arizona (Actor Extraction)/Arizona 2001 Pesticides Annual Report.py
+++ Arizona (Actor Extraction)/Arizona 2001 Pesticides Annual Report.py
@@ -13,20 +13,55 @@ import pandas as pd
 
 chemName = []
 casN = []
+prodID = []
+templateName = []
+msdsDate = []
+recUse = []
+catCode = []
+descrip = []
+code = []
+sourceType = []
 
 tables = (camelot.read_pdf(r'L:\Lab\HEM\ALarger\Actor Automated Extraction\Arizona\(2001) Pesticide Contamination Prevention Program Report A.R.S. 49-303.B/document_320430.pdf',pages='4-15', flavor='stream'))
 i=0 
 for table in tables:
     df = tables[i].df
-    if i <= 4:
+    nIngredients = len(df)
+    if i <= 4: #Table 2
         chemName.extend(df.loc[:,1])
         casN.extend(df.loc[:,0])
-    elif i >= 7:
+        prodID.extend(['1372074']*nIngredients)
+        templateName.extend(['Arizona 2001 Pesticide Report Table 2.pdf']*nIngredients)
+        msdsDate.extend([2001]*nIngredients)
+        recUse.extend(['']*nIngredients)
+        catCode.extend(['']*nIngredients)
+        descrip.extend(['']*nIngredients)
+        code.extend(['']*nIngredients)
+        sourceType.extend(['ACToR Assays and Lists']*nIngredients)
+    elif i >= 9: #Table 5
         chemName.extend(df.loc[:,4])
-        casN.extend(['']*len(df))
+        casN.extend(['']*nIngredients)
+        prodID.extend(['1372076']*nIngredients)
+        templateName.extend(['Arizona 2001 Pesticide Report Table 5.pdf']*nIngredients)
+        msdsDate.extend([2001]*nIngredients)
+        recUse.extend(['']*nIngredients)
+        catCode.extend(['']*nIngredients)
+        descrip.extend(['']*nIngredients)
+        code.extend(['']*nIngredients)
+        sourceType.extend(['ACToR Assays and Lists']*nIngredients)
+    elif i >= 7: #Table 4
+        chemName.extend(df.loc[:,4])
+        casN.extend(['']*nIngredients)
+        prodID.extend(['1372075']*nIngredients)
+        templateName.extend(['Arizona 2001 Pesticide Report Table 4']*nIngredients)
+        msdsDate.extend([2001]*nIngredients)
+        recUse.extend(['']*nIngredients)
+        catCode.extend(['']*nIngredients)
+        descrip.extend(['']*nIngredients)
+        code.extend(['']*nIngredients)
+        sourceType.extend(['ACToR Assays and Lists']*nIngredients)
     i+=1
 
-chemList = [] #list of chem names with cas numbers (so duplicates without cas can be deleted)
 j = len(chemName)
 while j > 0: #go through list backwards, so it doesnt mess up the index if a row is deleted
     j-=1
@@ -38,31 +73,28 @@ while j > 0: #go through list backwards, so it doesnt mess up the index if a row
     if chemName[j] == '' or 'active ingredient' in chemName[j] or 'c_name' in chemName[j] or chemName[j] == '*':
         del chemName[j]
         del casN[j]
+        del prodID[j]
+        del templateName[j]
+        del msdsDate[j]
+        del recUse[j]
+        del catCode[j]
+        del descrip[j]
+        del code[j]
+        del sourceType[j]
         continue
     elif chemName[j] == 'isopropanol' or chemName[j] == 'isopropylamine salt' or chemName[j] == 'salt' or chemName[j] == 'methanearsonate' or (chemName[j] == 'thidiazuron' and chemName[j-1] == 'diuron +') or chemName[j] == 'ammonium salt':
         chemName[j-1] = chemName[j-1] + ' ' + chemName[j]
         del chemName[j]
         del casN[j]
+        del prodID[j]
+        del templateName[j]
+        del msdsDate[j]
+        del recUse[j]
+        del catCode[j]
+        del descrip[j]
+        del code[j]
+        del sourceType[j]
         continue
-    if chemName[j] not in chemList and casN[j] != '':
-        chemList.append(chemName[j])
-        
-k = len(chemName) - 1
-while k >= 0:
-    if chemName[k] in chemList and casN[k] == '':
-        del chemName[k]
-        del casN[k]
-    k-=1
-    
-nIngredients = len(chemName)
-prodID = [320430]*nIngredients
-templateName = ['Arizona_***']*nIngredients
-msdsDate = [2001]*nIngredients
-recUse = ['***']*nIngredients
-catCode = ['ACToR_Assays_***']*nIngredients
-descrip = ['***']*nIngredients
-code = ['Arizona_***']*nIngredients
-sourceType = ['ACToR Assays and Lists']*nIngredients
   
 df = pd.DataFrame({'data_document_id':prodID, 'data_document_filename':templateName, 'doc_date':msdsDate, 'raw_category':recUse, 'raw_cas':casN, 'raw_chem_name':chemName, 'cat_code':catCode, 'description_cpcat': descrip, 'cpcat_code':code, 'cpcat_sourcetype':sourceType})
 df=df.drop_duplicates()
