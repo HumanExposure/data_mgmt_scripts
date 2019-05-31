f1c45dd3b8b77c9cde9cc4df3d67d54b780d6b83
larger-allison <45430904+larger-allison@users.noreply.github.com>
2019-05-31 12:47:07 -0400

    Update NFLIS-Drug 2018.py

--- DEA/NFLIS-Drug 2018.py
+++ DEA/NFLIS-Drug 2018.py
@@ -22,9 +22,7 @@ for table in tables:
         if inChems == True:
             if 'Total' in row:
                 inChems = False
-                print('break')
                 break
-            print(i)
             if row == '': continue
             chemName.append(row)
             prodID.append(1373593+i)
@@ -49,5 +47,4 @@ sourceType = ['ACToR Assays and Lists']*nIngredients
 casN = ['']*nIngredients
   
 df = pd.DataFrame({'data_document_id':prodID, 'data_document_filename':templateName, 'doc_date':msdsDate, 'raw_category':recUse, 'raw_cas':casN, 'raw_chem_name':chemName, 'cat_code':catCode, 'description_cpcat': descrip, 'cpcat_code':code, 'cpcat_sourcetype':sourceType})
-df=df.drop_duplicates()
 df.to_csv(r'L:\Lab\HEM\ALarger\Actor Automated Extraction\DEA\NFLIS-Drug 2018 Midyear Report\NFLIS-Drug 2018.csv',index=False, header=True)
