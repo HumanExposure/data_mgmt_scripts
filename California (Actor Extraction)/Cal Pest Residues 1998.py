7fc2bc329e5bc9be142077472d43a461f28f3078
larger-allison <45430904+larger-allison@users.noreply.github.com>
2019-06-06 17:00:56 -0400

    Update Cal Pest Residues 1998.py

--- California (Actor Extraction)/Cal Pest Residues 1998.py
+++ California (Actor Extraction)/Cal Pest Residues 1998.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 chemName = []
 
-#tables = (camelot.read_pdf(r'L:\Lab\HEM\ALarger\Actor Automated Extraction\California\Cal Pesticide Residues\document_1359546.pdf',pages='1375-1832', flavor='stream')) 
+tables = (camelot.read_pdf(r'L:\Lab\HEM\ALarger\Actor Automated Extraction\California\Cal Pesticide Residues\document_1359546.pdf',pages='1375-1832', flavor='stream')) 
 
 for table in tables: 
     df = table.df
