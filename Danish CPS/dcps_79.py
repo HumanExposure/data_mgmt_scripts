7065db5703e8c8cf3faa2e2adbe163a387442090
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2019-06-21 08:48:38 -0400

    Update dcps_79.py

--- Danish CPS/dcps_79.py
+++ Danish CPS/dcps_79.py
@@ -6,7 +6,7 @@ import pandas as pd
 import string
 
 #Table 1.5
-temp=read_pdf("document_1374286.pdf", pages="22,23", lattice=True, pandas_options={'header': None})
+temp=read_pdf("document_1374394.pdf", pages="22,23", lattice=True, pandas_options={'header': None})
 temp["raw_chem_name"]=temp.iloc[:,1]
 temp=temp.drop([0,15])
 temp=temp.dropna(subset=["raw_chem_name"])
@@ -38,7 +38,7 @@ table_1_5=table_1_5.drop_duplicates()
 table_1_5=table_1_5.reset_index()
 table_1_5=table_1_5[["raw_chem_name"]]
 
-table_1_5["data_document_id"]="1374286"
+table_1_5["data_document_id"]="1374394"
 table_1_5["data_document_filename"]="DCPS_79_b.pdf"
 table_1_5["doc_date"]="2006"
 table_1_5["raw_category"]=""
@@ -51,7 +51,7 @@ table_1_5["report_funcuse"]=""
 table_1_5.to_csv("dcps_79_table_1_5.csv", columns=["data_document_id","data_document_filename","doc_date","raw_category","raw_cas","raw_chem_name","cat_code","description_cpcat","cpcat_code","cpcat_sourcetype","report_funcuse"], index=False)
 
 #Table 2.2
-table_2_2=read_pdf("document_1374287.pdf", pages="26", lattice=True, pandas_options={'header': None})
+table_2_2=read_pdf("document_1374395.pdf", pages="26", lattice=True, pandas_options={'header': None})
 table_2_2["raw_chem_name"]=table_2_2.iloc[1:,0]
 table_2_2["raw_cas"]=table_2_2.iloc[1:,1]
 table_2_2=table_2_2.dropna(subset=["raw_chem_name"])
@@ -62,7 +62,7 @@ for j in range(0, len(table_2_2)):
     table_2_2["raw_chem_name"].iloc[j]=str(table_2_2["raw_chem_name"].iloc[j]).strip().lower().replace(".","").replace("α","alpha").replace("β","beta")
     table_2_2["raw_chem_name"].iloc[j]=clean(str(table_2_2["raw_chem_name"].iloc[j]))
 
-table_2_2["data_document_id"]="1374287"
+table_2_2["data_document_id"]="1374395"
 table_2_2["data_document_filename"]="DCPS_79_c.pdf"
 table_2_2["doc_date"]="2006"
 table_2_2["raw_category"]=""
@@ -76,7 +76,7 @@ table_2_2.to_csv("dcps_79_table_2_2.csv", columns=["data_document_id","data_docu
 
 
 #Table 2.4
-table_2_4=read_pdf("document_1374288.pdf", pages="28,29", lattice=True, pandas_options={'header': None})
+table_2_4=read_pdf("document_1374396.pdf", pages="28,29", lattice=True, pandas_options={'header': None})
 table_2_4["raw_chem_name"]=table_2_4.iloc[:,1]
 table_2_4["raw_cas"]=table_2_4.iloc[:,2]
 table_2_4["report_funcuse"]=table_2_4.iloc[1:,5]
@@ -93,7 +93,7 @@ for j in range(0, len(table_2_4)):
     if len(str(table_2_4["report_funcuse"].iloc[j]).split())>1:
         table_2_4["report_funcuse"].iloc[j]=" ".join(str(table_2_4["report_funcuse"].iloc[j]).split())
 
-table_2_4["data_document_id"]="1374288"
+table_2_4["data_document_id"]="1374396"
 table_2_4["data_document_filename"]="DCPS_79_d.pdf"
 table_2_4["doc_date"]="2006"
 table_2_4["raw_category"]=""
@@ -106,7 +106,7 @@ table_2_4.to_csv("dcps_79_table_2_4.csv", columns=["data_document_id","data_docu
 
 
 #Table 3.2
-tables=read_pdf("document_1374289.pdf", pages="31,32", lattice=True, multiple_tables=True, pandas_options={'header': None})
+tables=read_pdf("document_1374397.pdf", pages="31,32", lattice=True, multiple_tables=True, pandas_options={'header': None})
 table_3_2=pd.concat(tables[1:3], ignore_index=True)
 table_3_2["raw_chem_name"]=table_3_2.iloc[1:,0]
 table_3_2["raw_cas"]=table_3_2.iloc[1:,1]
@@ -122,7 +122,7 @@ for j in range(0, len(table_3_2)):
     if len(table_3_2["raw_chem_name"].iloc[j].split())>1:
         table_3_2["raw_chem_name"].iloc[j]=" ".join(table_3_2["raw_chem_name"].iloc[j].split())
 
-table_3_2["data_document_id"]="1374289"
+table_3_2["data_document_id"]="1374397"
 table_3_2["data_document_filename"]="DCPS_79_e.pdf"
 table_3_2["doc_date"]="2006"
 table_3_2["raw_category"]=""
@@ -136,7 +136,7 @@ table_3_2.to_csv("dcps_79_table_3_2.csv", columns=["data_document_id","data_docu
 
 
 #Table 3.4
-table_3_4=read_pdf("document_1374290.pdf", pages="33", lattice=True, pandas_options={'header': None})
+table_3_4=read_pdf("document_1374398.pdf", pages="33", lattice=True, pandas_options={'header': None})
 table_3_4["raw_chem_name"]=table_3_4.iloc[1:,0]
 table_3_4["raw_cas"]=table_3_4.iloc[1:,1]
 table_3_4=table_3_4.dropna(subset=["raw_chem_name"])
@@ -149,7 +149,7 @@ for j in range(0, len(table_3_4)):
     table_3_4["raw_chem_name"].iloc[j]=str(table_3_4["raw_chem_name"].iloc[j]).strip().lower().replace(".","").replace("*","")
     table_3_4["raw_chem_name"].iloc[j]=clean(str(table_3_4["raw_chem_name"].iloc[j]))
 
-table_3_4["data_document_id"]="1374290"
+table_3_4["data_document_id"]="1374398"
 table_3_4["data_document_filename"]="DCPS_79_f.pdf"
 table_3_4["doc_date"]="2006"
 table_3_4["raw_category"]=""
@@ -163,7 +163,7 @@ table_3_4.to_csv("dcps_79_table_3_4.csv", columns=["data_document_id","data_docu
 
 
 #Section 4 classifications
-sec_4=read_pdf("document_1374291.pdf", pages="35-37", lattice=True, pandas_options={'header': None})
+sec_4=read_pdf("document_1374399.pdf", pages="35-37", lattice=True, pandas_options={'header': None})
 sec_4["raw_chem_name"]=sec_4.iloc[1:,0]
 sec_4=sec_4.dropna(subset=["raw_chem_name"])
 sec_4=sec_4.reset_index()
@@ -184,7 +184,7 @@ sec_4=sec_4.reset_index()
 sec_4=sec_4[["raw_chem_name"]]
 
 
-sec_4["data_document_id"]="1374291"
+sec_4["data_document_id"]="1374399"
 sec_4["data_document_filename"]="DCPS_79_g.pdf"
 sec_4["doc_date"]="2006"
 sec_4["raw_category"]=""
