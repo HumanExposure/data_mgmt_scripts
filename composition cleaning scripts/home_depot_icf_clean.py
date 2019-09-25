d43e9792f0084cf2fa8c8b3d8f665c89e4ed6724
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2019-09-25 12:52:23 -0400

    Update home_depot_icf_clean.py

--- composition cleaning scripts/home_depot_icf_clean.py
+++ composition cleaning scripts/home_depot_icf_clean.py
@@ -11,60 +11,29 @@ os.chdir("//home//lkoval//comp_data_cleaning")
 raw_data=pd.read_csv("home_depot_-_icf_msds_extract_raw_extracted_records.csv", usecols=["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type__title"])
 
 raw_data=raw_data.loc[raw_data.unit_type__title=="percent"]
-raw_data=raw_data.loc[raw_data.raw_central_comp!="Balance"]
-raw_data=raw_data.loc[raw_data.raw_central_comp!="Up to 100"]
-raw_data=raw_data.loc[raw_data.raw_central_comp.astype(str).str.contains(",")==False]
+raw_data=raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & raw_data.raw_central_comp.str.replace(" ","").str.contains("[A-Za-z,]")==False]
 
 raw_data=raw_data.reset_index()
-raw_data=raw_data[["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type__title"]]
+raw_data=raw_data[["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp"]]
 
-cleaning_df=raw_data
-chemID=[]
-lower=[]
-central=[]
-upper=[]
-double=[]
-drop=[]
+raw_data["raw_central_comp"]=raw_data["raw_central_comp"].str.replace(' ','').str.strip()
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("-")), ["raw_min_comp"]]=raw_data["raw_central_comp"].str.split('\-', expand=True)[0]
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("-")), ["raw_max_comp"]]=raw_data["raw_central_comp"].str.split('\-', expand=True)[1]
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("-")), ["raw_central_comp"]]=""
 
-for index, row in cleaning_df.iterrows():
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("<")), ["raw_min_comp"]]="0"
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("<")), ["raw_max_comp"]]=raw_data["raw_central_comp"].str.strip("<")
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains("<")), ["raw_central_comp"]]=""
 
-    if pd.isnull(row.raw_min_comp) and pd.isnull(row.raw_central_comp) and pd.isnull(row.raw_max_comp):
-        continue
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains(">")), ["raw_min_comp"]]=raw_data["raw_central_comp"].str.strip(">")
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains(">")), ["raw_max_comp"]]="100"
+raw_data.loc[(pd.isnull(raw_data.raw_central_comp)==False) & (raw_data["raw_central_comp"].str.contains(">")), ["raw_central_comp"]]=""
 
-    elif pd.isnull(row.raw_min_comp)==False and pd.isnull(row.raw_central_comp):
-        chemID.append(str(row.ExtractedChemical_id))
-        lower.append(str(row.raw_min_comp))
-        upper.append(str(row.raw_max_comp))
-        central.append("")
+split_data=raw_data.rename(columns={"raw_min_comp":"lower_wf_analysis","raw_central_comp":"central_wf_analysis","raw_max_comp":"upper_wf_analysis"})
 
-    else:
-        chemID.append(str(row.ExtractedChemical_id))
-        flag=True
-        if "-" in row.raw_central_comp:
-            lower.append(row.raw_central_comp[:row.raw_central_comp.index("-")])
-            upper.append(row.raw_central_comp[row.raw_central_comp.index("-")+1:])
-            central.append("")
-            flag=False
-
-        if flag==True:
-            if row.raw_central_comp[0]=="<":
-                lower.append("0.0")
-                upper.append(row.raw_central_comp)
-                central.append("")
-            elif row.raw_central_comp[0]==">":
-                lower.append(row.raw_central_comp)
-                upper.append("100.0")
-                central.append("")
-            else:
-                lower.append("")
-                upper.append("")
-                central.append(row.raw_central_comp)
-
-split_data=pd.DataFrame()
-split_data["id"]=chemID
-split_data["lower_wf_analysis"]=lower
-split_data["central_wf_analysis"]=central
-split_data["upper_wf_analysis"]=upper
+split_data.loc[(pd.isnull(split_data.lower_wf_analysis)==False), ["lower_wf_analysis"]]=split_data["lower_wf_analysis"].astype(str)
+split_data.loc[(pd.isnull(split_data.central_wf_analysis)==False), ["central_wf_analysis"]]=split_data["central_wf_analysis"].astype(str)
+split_data.loc[(pd.isnull(split_data.upper_wf_analysis)==False), ["upper_wf_analysis"]]=split_data["upper_wf_analysis"].astype(str)
 
 split_data.lower_wf_analysis=pd.to_numeric(split_data.lower_wf_analysis.str.replace(">","").str.replace("<","").str.replace("=",""), errors='coerce')/100
 split_data.lower_wf_analysis=split_data.lower_wf_analysis.round(6)
@@ -74,4 +43,4 @@ split_data.central_wf_analysis=pd.to_numeric(split_data.central_wf_analysis.str.
 split_data.central_wf_analysis=split_data.central_wf_analysis.round(6)
 clean_data=split_data.fillna("")
 
-clean_data.to_csv("home_depot_icf_msds_all_cleaned.csv", index=False)
+clean_data.to_csv("home_depot_icf_msds_extract_cleaned.csv", index=False)
