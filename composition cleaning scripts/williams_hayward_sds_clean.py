c9c3c0ed606870308797793b64ed3ea407b90f41
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2020-02-24 15:43:14 -0500

    Add files via upload

---
+++ composition cleaning scripts/williams_hayward_sds_clean.py
@@ -0,0 +1,30 @@
+#lkoval
+#2-24-2020
+
+import pandas as pd
+import string
+import os
+
+os.chdir("//home//lkoval//comp_data_cleaning")
+raw_data=pd.read_csv("williams_hayward_protective_coatings_sds_raw_extracted_records.csv", usecols=["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type__title"], dtype="str")
+raw_data=raw_data.loc[raw_data.unit_type__title=="percent"]
+raw_data=raw_data.fillna("")
+
+split_data=raw_data
+split_data.loc[split_data.raw_central_comp.str.contains("<"), ["raw_max_comp"]]="1"
+split_data.loc[split_data.raw_central_comp.str.contains("<"), ["raw_min_comp"]]="0"
+split_data.loc[split_data.raw_central_comp.str.contains("<"), ["raw_central_comp"]]=""
+
+clean_data=split_data.rename(columns={"raw_min_comp":"lower_wf_analysis","raw_central_comp":"central_wf_analysis","raw_max_comp":"upper_wf_analysis"})
+clean_data.lower_wf_analysis=pd.to_numeric(clean_data.lower_wf_analysis)/100
+clean_data.lower_wf_analysis=clean_data.lower_wf_analysis.round(10)
+clean_data.central_wf_analysis=pd.to_numeric(clean_data.central_wf_analysis)/100
+clean_data.central_wf_analysis=clean_data.central_wf_analysis.round(10)
+clean_data.upper_wf_analysis=pd.to_numeric(clean_data.upper_wf_analysis)/100
+clean_data.upper_wf_analysis=clean_data.upper_wf_analysis.round(10)
+
+clean_data=clean_data.fillna("")
+clean_data=clean_data.applymap(str)
+clean_data=clean_data[["ExtractedChemical_id","lower_wf_analysis","central_wf_analysis","upper_wf_analysis"]]
+
+clean_data.to_csv("williams_hayward_protective_coatings_sds_cleaned.csv", index=False)
