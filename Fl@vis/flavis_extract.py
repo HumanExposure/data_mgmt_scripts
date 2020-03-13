5d239ae2593d1201139b6d54c7c792ee4372f5a5
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2020-03-13 09:24:03 -0400

    Add files via upload

---
+++ Fl@vis/flavis_extract.py
@@ -0,0 +1,32 @@
+#lkoval
+#3-13-20
+
+import pandas as pd
+import os
+import string
+
+os.chdir("//home//lkoval//FUse")
+
+clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty)) #clean function to remove non-printable characters
+
+flavis=pd.read_excel("flavis.xlsx", usecols=["Flavis name","CAS No"], dtype=str)
+flavis=flavis.rename(columns={"Flavis name":"raw_chem_name","CAS No":"raw_cas"})
+flavis=flavis.dropna(how="all").drop_duplicates(subset=["raw_chem_name"]).fillna("")
+
+#some cas reading as dates with time stamp. Remove the time stamp
+flavis.loc[flavis.raw_cas.str.contains("\d+-\d{2}-\d{1}"), ["raw_cas"]]=flavis.raw_cas.str.extract(r'(\d+-\d{2}-\d{1})', expand=False)
+flavis.raw_chem_name=flavis.raw_chem_name.apply(clean)
+
+flavis["data_document_id"]=""
+flavis["data_document_filename"]="flavis.pdf"
+flavis["doc_date"]="2020"
+flavis["raw_category"]=""
+flavis["cat_code"]=""
+flavis["description_cpcat"]=""
+flavis["cpcat_sourcetype"]=""
+flavis["report_funcuse"]=""
+flavis["component"]=""
+
+flavis=flavis[["data_document_id","data_document_filename","doc_date","raw_category","raw_cas","raw_chem_name","cat_code","description_cpcat","cpcat_sourcetype","report_funcuse","component"]]
+
+flavis.to_csv("flavis.csv", index=False)
