2673642b40cad55f86af9a6421903ed743387c73
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2020-05-04 14:58:02 -0400

    Update list_n_apr10_-_apr24_clean.py

--- composition cleaning scripts/list_n_apr10_-_apr24_clean.py
+++ composition cleaning scripts/list_n_apr10_-_apr24_clean.py
@@ -65,3 +65,20 @@ split_data.central_wf_analysis=split_data.central_wf_analysis.round(10)
 
 clean_data=split_data.copy()
 clean_data.to_csv("list_n_apr24_2020_cleaned.csv", index=False)
+
+
+"""
+May 1 2020
+"""
+
+raw_data=pd.read_csv("epa_list_n_-_may_1_2020_raw_extracted_records.csv", usecols=["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type__title"], dtype=str)
+raw_data=raw_data.loc[raw_data.unit_type__title=="percent"]
+raw_data=raw_data.fillna("")
+split_data=raw_data.rename(columns={"raw_min_comp":"lower_wf_analysis","raw_central_comp":"central_wf_analysis","raw_max_comp":"upper_wf_analysis"})
+split_data=split_data[["ExtractedChemical_id","lower_wf_analysis","central_wf_analysis","upper_wf_analysis"]]
+
+split_data.central_wf_analysis=pd.to_numeric(split_data.central_wf_analysis)/100
+split_data.central_wf_analysis=split_data.central_wf_analysis.round(10)
+
+clean_data=split_data.copy()
+clean_data.to_csv("list_n_may1_2020_cleaned.csv", index=False)
