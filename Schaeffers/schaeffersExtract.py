698333fb33c1712213d2aac83bc8d4eae3f5665a
larger-allison <45430904+larger-allison@users.noreply.github.com>
2020-06-17 16:34:17 -0400

    Update schaeffersExtract.py

--- Schaeffers/schaeffersExtract.py
+++ Schaeffers/schaeffersExtract.py
@@ -61,21 +61,21 @@ def extractData(fileList):
     then generates a csv to upload into factotum
     file_list: a list of the txt file names in the data group
     """
-    idList = [] #list of product IDs
-    filenameList = [] #list of file names matching those in the extacted text template
-    prodnameList = [] #list of product names
-    dateList = [] #list of msdsDates
-    revList = [] #list of revision numbers
-    catList = [] #list of product categories
-    casList = [] #list of CAS numbers
-    chemList = [] #list of chemical names
-    useList = [] #list of functional uses of each chemical
-    minList = [] #list of minimum concentrations
-    maxList = [] #list of maximum concentrations
-    unitList = [] #list of unit types (1=weight frac, 2=unknown, 3=weight percent,...)
-    rankList = [] #list of ingredient ranks
-    centList = [] #list of central concentrations
-    componentList = [] #List of components
+    idList = [] #Factotum document IDs
+    filenameList = [] #file names
+    prodnameList = [] #product names
+    dateList = [] #sds dates
+    revList = [] #revision numbers
+    catList = [] #product categories
+    casList = [] #CAS numbers
+    chemList = [] #chemical names
+    useList = [] #functional uses of each chemical
+    minList = [] #minimum concentrations
+    maxList = [] #maximum concentrations
+    unitList = [] #unit type codes (1=weight frac, 2=unknown, 3=weight percent,...)
+    rankList = [] #ingredient ranks
+    centList = [] #central concentrations
+    componentList = [] #components
     
     for file in fileList:
         if file in ['147.txt','230.txt','289.txt','511.txt','741.txt','751M.txt','285.txt']:
@@ -95,11 +95,12 @@ def extractData(fileList):
         centC = []
         unit = []
                 
-        inUse = False
-        inIngredients = False
+        inUse = False #Flag for if you are in the Identified uses section of the SDS
+        inIngredients = False #Flag for if you are in the ingredients section of the SDS
         
-        template = csv.reader(open(r'schaeffer_manufacturing_co_safety_data_sheets_documents_20191122.csv'))
-        for row in template:
+        #Get Factotum IDs
+        docRecords = csv.reader(open('schaeffer_manufacturing_co_safety_data_sheets_documents_20191122.csv')) #csv downloaded from Factotum using the "Document records" button on the datagroup page
+        for row in docRecords:
             if row[3] == file.replace('.txt','.pdf'):
                 ID = row[0]
                 break
@@ -107,15 +108,18 @@ def extractData(fileList):
             continue
             
         for line in ifile:
-            
             cline = cleanLine(line)
-            if cline == '': continue
+            if cline == '': continue #Skip blank lines
+                
+            #Get product/document data
             if  'ghs product identifier' in cline and prodname == '':
                 prodname = cline.split(':')[-1].strip()
             if 'date of issue' in cline and date == '':
                 date = cline.split(':')[-1].strip()
             if 'version :' in cline:
                 rev = cline.split(':')[-1].strip()
+            
+            #Get product use
             if inUse == True:
                 if 'schaeffer' in cline:
                     inUse = False
@@ -124,43 +128,42 @@ def extractData(fileList):
             if 'identified uses' in cline:
                 cat = cline.split(':')[-1].strip()
                 inUse = True
+                
+            #Get ingredient data
             if inIngredients == True:
                 conc = ''
-                sline = splitLine(line)
+                sline = splitLine(line) #split line into a list of elements
                 if 'any concentration shown' in cline or 'section 4' in cline or 'united states' in cline:
-                    inIngredients = False
+                    inIngredients = False #out of ingredients section
                     
-                elif len(sline) == 3:
+                elif len(sline) == 3: #if the line has three elements, they are ingredient name, %, and cas number
                     chem.append(sline[0])
                     conc = sline[1]
                     cas.append(sline[2])
                     if 'see below' in cas[-1]:
                         cas[-1] = ''
 
-                    if conc.count('-') == 1:
+                    if conc.count('-') == 1: #if there is a dash in the concentration, everything before it is the minimun concentration, everything after it is the maximum concentration, and the central concentration is blank
                         minC.append(conc.split('-')[0])
                         maxC.append(conc.split('-')[1])
                         centC.append('')
                         unit.append(3)
-                    elif conc == '':
-                        minC.append('')
-                        maxC.append('')
-                        centC.append('')
-                        unit.append('')
-                    else:
+                    else: #if there is no dash in the concentration, the concentration should go in the central concentration and the minimum and maximum concentrations should be blank 
                         minC.append('')
                         maxC.append('')
                         centC.append(conc)
                         unit.append(3)
                                         
-                elif len(sline) == 1:
+                elif len(sline) == 1: #if the line only has one element, it is a continuation of the chemical name from the previous line 
                     chem[-1] = chem[-1] + ' ' + sline[0]
+                    
                 else:
                     print(len(sline),sline, file)
+                    
             if 'ingredient name' in cline and 'cas' in cline: 
                 inIngredients = True
                 
-        if chem == []:
+        if chem == []: #if a document has no chemicals in it, leave all chemical related fields blank
             chem = ['']
             cas = ['']
             minC = ['']
@@ -168,6 +171,7 @@ def extractData(fileList):
             centC = ['']
             unit = ['']
             
+        #add the data from this document to the 
         n = len(chem)
         idList.extend([ID]*n)
         filenameList.extend([file.replace('.txt','.pdf')]*n)
@@ -183,12 +187,13 @@ def extractData(fileList):
         unitList.extend(unit)
         centList.extend(centC)
         componentList.extend([component]*n)
-        
+        #add rank
         if chem == ['']:
             rankList.extend([''])
         else:
             rankList.extend(list(range(1,n+1)))
    
+    #create csv
     df = pd.DataFrame({'data_document_id':idList, 'data_document_filename':filenameList, 'prod_name':prodnameList, 'doc_date':dateList, 'rev_num':revList, 'raw_category':catList, 'raw_cas':casList, 'raw_chem_name':chemList, 'report_funcuse':useList, 'raw_min_comp': minList, 'raw_max_comp':maxList, 'unit_type':unitList, 'ingredient_rank':rankList, 'raw_central_comp':centList, 'component':componentList})
     df.to_csv(r'Schaeffer Extracted Text.csv',index=False, header=True)
     
