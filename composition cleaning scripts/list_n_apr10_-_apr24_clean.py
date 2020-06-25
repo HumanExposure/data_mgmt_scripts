537a0f4ba5ccedcf869f06246e0ad821e3ec0d23
Lauren Koval <45081529+l-koval@users.noreply.github.com>
2020-06-25 08:04:54 -0400

    Update list_n_apr10_-_apr24_clean.py

--- composition cleaning scripts/list_n_apr10_-_apr24_clean.py
+++ composition cleaning scripts/list_n_apr10_-_apr24_clean.py
@@ -118,3 +118,36 @@ split_data.central_wf_analysis=split_data.central_wf_analysis.round(10)
 clean_data=split_data.copy()
 clean_data.to_csv("list_n_may22_2020_cleaned.csv", index=False)
 
+
+"""
+Jun 5 2020
+"""
+
+raw_data=pd.read_csv("epa_list_n_-_june_5_2020_raw_extracted_records.csv", usecols=["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type"], dtype=str)
+raw_data=raw_data.loc[raw_data.unit_type=="percent"]
+raw_data=raw_data.fillna("")
+split_data=raw_data.rename(columns={"raw_min_comp":"lower_wf_analysis","raw_central_comp":"central_wf_analysis","raw_max_comp":"upper_wf_analysis"})
+split_data=split_data[["ExtractedChemical_id","lower_wf_analysis","central_wf_analysis","upper_wf_analysis"]]
+
+split_data.central_wf_analysis=pd.to_numeric(split_data.central_wf_analysis)/100
+split_data.central_wf_analysis=split_data.central_wf_analysis.round(10)
+
+clean_data=split_data.copy()
+clean_data.to_csv("list_n_june5_2020_cleaned.csv", index=False)
+
+
+"""
+Jun 19 2020
+"""
+
+raw_data=pd.read_csv("epa_list_n_-_june_19_2020_raw_extracted_records.csv", usecols=["ExtractedChemical_id","raw_min_comp","raw_central_comp","raw_max_comp","unit_type"], dtype=str)
+raw_data=raw_data.loc[raw_data.unit_type=="percent"]
+raw_data=raw_data.fillna("")
+split_data=raw_data.rename(columns={"raw_min_comp":"lower_wf_analysis","raw_central_comp":"central_wf_analysis","raw_max_comp":"upper_wf_analysis"})
+split_data=split_data[["ExtractedChemical_id","lower_wf_analysis","central_wf_analysis","upper_wf_analysis"]]
+
+split_data.central_wf_analysis=pd.to_numeric(split_data.central_wf_analysis)/100
+split_data.central_wf_analysis=split_data.central_wf_analysis.round(10)
+
+clean_data=split_data.copy()
+clean_data.to_csv("list_n_june19_2020_cleaned.csv", index=False)
