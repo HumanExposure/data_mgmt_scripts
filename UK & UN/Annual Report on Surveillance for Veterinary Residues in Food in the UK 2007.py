1a3564d63a8a21d01797ca2a7919672ec3363f89
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2023-12-04 14:26:59 -0500

    Update Annual Report on Surveillance for Veterinary Residues in Food in the UK 2007.py

--- UK & UN/Annual Report on Surveillance for Veterinary Residues in Food in the UK 2007.py
+++ UK & UN/Annual Report on Surveillance for Veterinary Residues in Food in the UK 2007.py
@@ -1,1 +1,10 @@
 #CSV was extracted using Tabula's web application
+import pandas as pd
+import string
+import os
+import re
+os.chdir(r'C:\Users\mmetcalf\Documents and Scripts\UK & UN')
+table = pd.read_csv('Annual Report on Surveillance for Veterinary Residues in Food in the UK 2007.csv')
+table.drop_duplicates()
+table["description_cpcat"]=""
+table.to_csv("Annual Report on Surveillance for Veterinary Residues in Food in the UK 2007.csv", columns=["data_document_id","data_document_filename","doc_date","raw_category","raw_cas","raw_chem_name","report_funcuse","cat_code","cpcat_code","description_cpcat","cpcat_sourcetype","component","chem_detected_flag","author","doi"], index=False)
