05ea7bd25a599b5f7e69b1c9a6ef3ab11731aa70
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2023-12-05 13:02:14 -0500

    Update UK & UN.py

--- UK & UN.py
+++ UK & UN.py
@@ -12,3 +12,7 @@ table.to_csv("Annual Report on Surveillance for Veterinary Residues in Food in t
 table2 = pd.read_csv('Annual Report on Surveillance for Veterinary Residues in Food in the UK 2008.csv')
 table2.drop_duplicates(inplace=True)
 table2.to_csv("Annual Report on Surveillance for Veterinary Residues in Food in the UK 2008.csv", columns=["data_document_id","data_document_filename","doc_date","raw_category","raw_cas","raw_chem_name","report_funcuse","cat_code","description_cpcat","cpcat_code","cpcat_sourcetype","component","chem_detected_flag","author","doi"], index=False)
+
+table3 = pd.read_csv('Annual Report on Surveillance for Veterinary Residues in Food in the UK 2009.csv')
+table3.drop_duplicates("raw_chem_name",inplace=True)
+table3.to_csv("Annual Report on Surveillance for Veterinary Residues in Food in the UK 2009.csv", columns=["data_document_id","data_document_filename","doc_date","raw_category","raw_cas","raw_chem_name","report_funcuse","cat_code","description_cpcat","cpcat_code","cpcat_sourcetype","component","chem_detected_flag","author","doi"], index=False)
