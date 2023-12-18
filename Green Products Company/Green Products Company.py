a39eb5a4cd3ebfa011b829d5c14480c553192288
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2023-12-18 14:41:12 -0500

    Update Green Products Company.py

--- Green Products Company/Green Products Company.py
+++ Green Products Company/Green Products Company.py
@@ -1,5 +1,6 @@
 #Michael Metcalf
 #12/14/2023
+#%%
 from tabula import read_pdf
 import pandas as pd
 import string
@@ -7,13 +8,15 @@ import os
 import re
 pd.options.mode.chained_assignment = None
 os.chdir(r'C:\Users\mmetcalf\Documents and Scripts\Green Products Company\Green Products Company Documents')
+
+#Copper-Green Brown
 tableCGB=read_pdf("Copper-Green Brown.pdf", pages="3", lattice=False, pandas_options={'header': None})[0]
 tableCGB.drop([0,1],axis=0,inplace=True)
 tableCGB.drop([1],axis=1,inplace=True)
 tableCGB.reset_index(drop=True,inplace=True)
 tableCGB["data_document_id"]="1687044"
 tableCGB["data_document_filename"] = "Copper-Green Brown.pdf"
-tableCGB["prod_name"] = "Copper-Green Wood Preservative"
+tableCGB["prod_name"] = "Copper-Green Brown Wood Preservative"
 tableCGB["doc_date"] = "19-Oct-2022"
 tableCGB["rev_num"] = ""
 tableCGB["raw_category"] = "wood perservative"
@@ -38,4 +41,28 @@ tableCGB.loc[1,"ingrediant_rank"] = 2
 tableCGB.loc[2,"ingrediant_rank"] = 3
 tableCGB["raw_central_comp"] = ""
 tableCGB["component"] = ""
-tableCGB.to_csv("Copper-Green Brown.csv", columns=["data_document_id","data_document_filename","doc_date","rev_num","raw_category","raw_cas","raw_chem_name","report_funcuse","raw_min_comp","raw_max_comp","unit_type","ingrediant_rank","raw_central_comp","component"], index=False)
+tableCGB.to_csv("Copper-Green Brown.csv", columns=["data_document_id","data_document_filename","prod_name","doc_date","rev_num","raw_category","raw_cas","raw_chem_name","report_funcuse","raw_min_comp","raw_max_comp","unit_type","ingrediant_rank","raw_central_comp","component"], index=False)
+
+#Copper-Green
+tableCG = tableCGB
+tableCG["data_document_id"] = "1687045"
+tableCG["data_document_filename"] = "Copper-Green.pdf"
+tableCG["prod_name"] = "Copper-Green Wood Perservative"
+tableCG.to_csv("Copper-Green.csv", columns=["data_document_id","data_document_filename","prod_name","doc_date","rev_num","raw_category","raw_cas","raw_chem_name","report_funcuse","raw_min_comp","raw_max_comp","unit_type","ingrediant_rank","raw_central_comp","component"], index=False)
+
+#Green's Water-Based
+tableGWB=read_pdf("Green_s Water-Based.pdf", pages="3", lattice=False, pandas_options={'header': None})[0]
+tableGWB.drop([0],axis=0,inplace=True)
+tableGWB.drop([1],axis=1,inplace=True)
+tableGWB.reset_index(drop=True,inplace=True)
+tableGWB["data_document_id"]="1687046"
+tableGWB["data_document_filename"] = "Green_s Water-Based.pdf"
+tableGWB["prod_name"] = "Green's Water-Based Wood Preservative"
+tableGWB["doc_date"] = "06-Dec-2022"
+tableGWB["rev_num"] = ""
+tableGWB["raw_category"] = "wood perservative"
+tableGWB["raw_cas"] = tableGWB.iloc[:,1]
+tableGWB["raw_chem_name"] = tableGWB.iloc[:,0]
+tableGWB=tableGWB.dropna(subset=["raw_chem_name"])
+for j in range(0, len(tableGWB)):
+    tableGWB["raw_chem_name"].iloc[j]=str(tableGWB["raw_chem_name"].iloc[j]).strip().lower().replace("*","")
