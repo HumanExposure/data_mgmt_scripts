34fd51470ccacc3b61392fc8767440b8565cf285
cnlutz20 <73549187+cnlutz20@users.noreply.github.com>
2024-01-24 13:42:57 -0500

    Extraction File for 2009 report added
    file used to extract tables 3,5,6, and 7 from 2009 pesticide monitoring reports

---
+++ FDA (Actor Extraction)/2009_pesticide_monitoring_ext.py
@@ -0,0 +1,109 @@
+
+# %% imports
+import camelot
+import os, string, re
+import pandas as pd
+import numpy as np
+from tabula import read_pdf
+from glob import glob
+# %% Definitions
+
+
+import camelot, string
+import pandas as pd
+
+
+
+# %%
+
+
+os.chdir(r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\Pesticide Residue Monitoring\2009")
+
+
+file_names = pd.read_csv("filenames.csv")
+file_names = file_names["filename"].to_list()
+
+
+data = os.path.abspath("pdfs/")
+
+for i, f in enumerate(os.listdir(data)):
+    
+    file = file_names[i]
+    src = os.path.join(data, f)
+    dst = os.path.join(data, file)
+    os.rename(src, dst)
+
+
+
+# %%
+
+chemName = []
+prodID = []
+templateName = []
+
+clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty))
+
+tables = camelot.read_pdf(r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\Pesticide Residue Monitoring\2009\2009.pdf",pages='20-31', flavor='lattice')
+
+
+table_0 = tables[0].df
+
+tables_ls = []
+
+for i,table in enumerate(tables):
+    print('table: '+str(i))
+    print(table.df)
+    print("##############")
+    print('\n')
+
+
+for i,table in enumerate(tables):
+    df = tables[i].df
+    if i <= 3: #Table 3
+        chemName.extend(df.iloc[:,0])
+        chemName.extend(df.iloc[:,1])
+        chemName.extend(df.iloc[:,2])
+        prodID.extend(['1690488']*len(df)*3)
+        templateName.extend(['Pesticide Residue Monitoring 2009 Report Table 3.pdf']*len(df)*3)
+    elif i == 5: #Table 5
+        chemName.extend(df.iloc[:,0])
+        prodID.extend(['1690489']*len(df))
+        templateName.extend(['Pesticide Residue Monitoring 2008 Report Table 5.pdf']*len(df))
+    elif i == 6: #Table 6
+        chemName.extend(df.iloc[:,0])
+        prodID.extend(['1690490']*len(df))
+        templateName.extend(['Pesticide Residue Monitoring 2008 Report Table 6.pdf']*len(df))
+    elif i == 7: #Table 7
+        chemName.extend(df.iloc[:,0])
+        prodID.extend(['1690491']*len(df))
+        templateName.extend(['Pesticide Residue Monitoring 2008 Report Table 7.pdf']*len(df))
+    
+    
+j = len(chemName)
+while j > 0: #go through list backwards, so it doesnt mess up the index if a row is deleted
+    j-=1
+    chemName[j] = chemName[j].replace('\n',' ').replace('ǂ', '').replace('*', '').rstrip('+').strip().rstrip('*').strip().lower().replace('  ',' ')
+    if prodID[j] != '1690488': chemName[j] = chemName[j].rstrip('2').rstrip('3').rstrip('4').rstrip('5')
+    if chemName[j] == '' or 'pesticide' in chemName[j] or chemName[j] == 'all others':
+        del chemName[j]
+        del templateName[j]
+        del prodID[j]
+        
+nIngredients = len(chemName)
+msdsDate = ['']*nIngredients
+recUse = ['']*nIngredients
+catCode = ['']*nIngredients
+descrip = ['']*nIngredients
+code = ['']*nIngredients 
+sourceType = ['ACToR Assays and Lists']*nIngredients
+casN = ['']*nIngredients 
+  
+df = pd.DataFrame({'data_document_id':prodID, 'data_document_filename':templateName, 'doc_date':msdsDate, 'raw_category':recUse, 'raw_cas':casN, 'raw_chem_name':chemName, 'cat_code':catCode, 'description_cpcat': descrip, 'cpcat_code':code, 'cpcat_sourcetype':sourceType})
+
+# %%
+
+
+
+os.chdir(r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\Pesticide Residue Monitoring\2009")
+df.to_csv(r'2009_pesticide_monitoring_ext.csv',index=False, header=True, date_format=None)
+# %%
