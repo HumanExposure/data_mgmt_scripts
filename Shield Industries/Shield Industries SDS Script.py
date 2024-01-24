fa5d9f4e304e70f1aea2078f50474f9c63e758a6
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2024-01-24 12:04:06 -0500

    Update Shield Industries SDS Script.py

--- Shield Industries/Shield Industries SDS Script.py
+++ Shield Industries/Shield Industries SDS Script.py
@@ -11,11 +11,6 @@ df = pd.DataFrame(columns=['filename','title'])
 
 for pdf_file in pdf_files:
     with open(os.path.join(pdf_directory,pdf_file), 'rb') as file:
-        if pdf_file == "TotalCare-Green-Carpet-Stain-Soil-Remover-RTU.pdf":
-            product_name = "TOTALCARE® “Green” Carpet STAIN & SOIL Remover"
-            new_row = pd.DataFrame({'filename': [pdf_file], 'title': [product_name]})
-            df = pd.concat([df,new_row], ignore_index=True)
-        else:
             reader = PyPDF2.PdfFileReader(file)
             page = reader.getPage(0)
             text = page.extractText()
@@ -25,7 +20,6 @@ for pdf_file in pdf_files:
             new_row = pd.DataFrame({'filename': [pdf_file], 'title': [product_name]})
             df = pd.concat([df,new_row], ignore_index=True)
 df['document_type'] = "SD"
-df.loc[46,'document_type'] = "TD"
 df['url'] = ""
 df["organization"] = "Shield Industries"
 df["subtitle"] = ""
@@ -34,4 +28,3 @@ df["pmid"] = ""
 df["hero_id"] = ""
 csv_file = r'C:\Users\mmetcalf\Documents and Scripts\Shield Industries\Shield Industries CSV.csv'
 df.to_csv(csv_file,index=False)
-
