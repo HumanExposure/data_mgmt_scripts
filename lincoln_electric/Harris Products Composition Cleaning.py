546551fdcbf61a589b805ebdd1eeb2b8ad1175b3
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2024-01-30 11:47:54 -0500

    Update Harris Products Composition Cleaning.py

--- lincoln_electric/Harris Products Composition Cleaning.py
+++ lincoln_electric/Harris Products Composition Cleaning.py
@@ -32,6 +32,8 @@ for index, row in df.iterrows():
         df.at[index, 'lower_wf_analysis'] = x / 100
         df.at[index, 'upper_wf_analysis'] = y / 100
         df.at[index, 'central_wf_analysis'] = ''
+    elif math.isnan(row['raw_min_comp']) and math.isnan(row['raw_max_comp']) and math.isnan(row['raw_central_comp']):
+        df.drop(index, inplace=True)
 
 df = df[['ExtractedComposition_id', 'lower_wf_analysis', 'central_wf_analysis', 'upper_wf_analysis']]
 
