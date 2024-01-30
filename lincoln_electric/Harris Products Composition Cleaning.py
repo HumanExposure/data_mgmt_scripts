0280972d19e851b8f945f9b9832796be45a712ba
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2024-01-30 09:58:53 -0500

    Add files via upload

---
+++ lincoln_electric/Harris Products Composition Cleaning.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import numpy as np
+import math
+
+pd.options.mode.chained_assignment = None
+os.chdir(r'C:\Users\mmetcalf\Documents and Scripts\Harris Products\Composition Cleaning')
+df = pd.read_csv('Factotum_Harris_Products_Group_SDS_raw_extracted_records_20240130.csv')
+
+
+for index, row in df.iterrows():
+    if str(row['raw_min_comp']).isnumeric() and str(row['raw_max_comp']).isnumeric():
+        x = row['raw_min_comp']
+        y = row['raw_max_comp']
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '<' in str(row['raw_max_comp']):
+        x = float(str(row['raw_max_comp']).replace('<', '').strip())
+        y = float(row['raw_min_comp'])
+        df.at[index, 'lower_wf_analysis'] = y / 100
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '<' in str(row['raw_central_comp']):
+        x = float(str(row['raw_central_comp']).replace('<', '').strip())
+        df.at[index, 'lower_wf_analysis'] = 0
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif not math.isnan(float(row['raw_min_comp'])) and not math.isnan(float(row['raw_max_comp'])):
+        x = float(row['raw_min_comp'])
+        y = float(row['raw_max_comp'])
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+
+df = df[['ExtractedComposition_id', 'lower_wf_analysis', 'central_wf_analysis', 'upper_wf_analysis']]
+
+df = df.fillna('')
+
+df.to_csv('Harris_Products_Cleaned_Data.csv', index=False)
