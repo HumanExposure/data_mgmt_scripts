c1457888173eaff65b7a392da4ec7dfad02ec7ac
cnlutz20 <73549187+cnlutz20@users.noreply.github.com>
2024-02-23 16:55:20 -0500

    uploading ext files
    2012-2016 ext files uploaded

---
+++ FDA (Actor Extraction)/CDER Drug and Biologic Approvals 2013.py
@@ -0,0 +1,94 @@
+
+# %% imports
+
+import os, string, re, camelot
+import pandas as pd
+import numpy as np
+
+
+
+# %%renaming files in pdf folder
+
+
+# os.chdir(r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\CDER Drug and Biologic Approvals\2013")
+
+
+# file_names = pd.read_csv("filenames.csv")
+# file_names = file_names["filename"].to_list()
+
+
+# data = os.path.abspath("pdfs/")
+
+# for i, f in enumerate(os.listdir(data)):
+    
+#     file = file_names[i]
+#     src = os.path.join(data, f)
+#     dst = os.path.join(data, file)
+#     os.rename(src, dst)
+
+
+
+# %%
+
+chemName = []
+prodID = []
+templateName = []
+components = []
+
+clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty))
+file = r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\CDER Drug and Biologic Approvals\2013\2013.pdf"
+tables = camelot.read_pdf(file,pages='all', flavor='lattice')
+
+
+tables_ls = []
+
+
+for i,table in enumerate(tables):
+    df = tables[i].df
+    if i < 3: #New Drug Approvals
+        chemName.extend(df.iloc[:,2])
+        prodID.extend(['1690581']*len(df))
+        templateName.extend(['cder_drug_and_biological_approvals_2013_nda.pdf']*len(df))
+        components.extend(['']*len(df))
+    else: #Table 7
+        chemName.extend(df.iloc[:,2])
+        prodID.extend(['1690582']*len(df))
+        templateName.extend(['cder_drug_and_biological_approvals_2013_bla.pdf']*len(df))
+        components.extend(['']*len(df))
+        
+j = len(chemName)
+while j > 0: #go through list backwards, so it doesnt mess up the index if a row is deleted
+    j-=1
+    chemName[j] = chemName[j].replace('\n',' ').rstrip('+').strip().rstrip('*').strip().lower().replace('  ',' ')
+    chemName[j] = re.sub(r'\(\d+\)$', '', chemName[j]).strip()
+    if prodID[j] != '1690581': chemName[j] = chemName[j].replace('ǂ', '').replace('*', '').rstrip('2').rstrip('3').rstrip('4').rstrip('5')
+    if chemName[j] == '' or 'proper name' in chemName[j] or 'established' in chemName[j] or chemName[j] == 'all others':
+        del chemName[j]
+        del templateName[j]
+        del prodID[j]
+        del components[j]
+
+        
+nIngredients = len(chemName)
+msdsDate = ['']*nIngredients
+recUse = ['']*nIngredients
+catCode = ['']*nIngredients
+descrip = ['']*nIngredients
+code = ['']*nIngredients 
+sourceType = ['ACToR Assays and Lists']*nIngredients
+casN = ['']*nIngredients 
+
+
+
+# %%
+
+df = pd.DataFrame({'data_document_id':prodID, 'data_document_filename':templateName, 'doc_date':msdsDate, 'raw_category':recUse, 'raw_cas':casN, 'raw_chem_name':chemName, 'cat_code':catCode, 'description_cpcat': descrip, 'cpcat_code':code, 'cpcat_sourcetype':sourceType, 'component': components})
+
+# %%
+
+
+
+os.chdir(r"C:\Users\CLUTZ01\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Projects\Extraction Scripts\FDA\CDER Drug and Biologic Approvals\2013")
+df.to_csv(r'2013_cder_drug_approvals_ext.csv',index=False, header=True, date_format=None)
+
+# %%
