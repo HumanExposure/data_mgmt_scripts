1a90ba0ae9150264c99d51ded333d83a7d80c15a
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2024-07-08 16:15:17 -0400

    Update item_300972.py

--- Walmart - in CPDat/item_300972.py
+++ Walmart - in CPDat/item_300972.py
@@ -1,4 +1,3 @@
-#%%
 from tabula import read_pdf
 import pandas as pd
 import numpy as np
@@ -23,15 +22,30 @@ while j < len(table)-1:
 
 table["raw_chem_name"] = table.iloc[:,0]
 table["raw_cas"] = table.iloc[:,1]
-table["raw_central_comp"] = table.iloc[2,0]
+table["raw_central_comp"] = table.iloc[:,2]
 for j in range(0, len(table)):
     table["raw_chem_name"].iloc[j]=str(table["raw_chem_name"].iloc[j]).strip().lower().replace("*","")
 
+#Code to add rank
 table["ingredient_rank"] = ""
 j = 0
-while j < len(table)-1:
+while j < len(table):
     table.at[j,"ingredient_rank"] = j + 1
     j = j + 1
+
+#Code to fix comp
+table["raw_min_comp"] = ""
+table["raw_max_comp"] = ""
+j = 0
+while j < len(table):
+    if "-" in str(table.loc[j,"raw_central_comp"]):
+        comp = str(table.loc[j,"raw_central_comp"]).split('-')
+        table.at[j,"raw_min_comp"] = comp[0].strip()
+        table.at[j,"raw_max_comp"] = comp[1].strip()
+        table.at[j,"raw_central_comp"] = ""
+        j = j + 1
+    else:
+        j = j + 1
 
 table.drop([0,1,2],axis=1,inplace=True)
 table["data_document_id"]="1764292"
@@ -40,6 +54,8 @@ table["prod_name"] = "JR Watkins Aloe & Green Tea Lotion"
 table["doc_date"]="12-Jan-2007"
 table["rev_num"] = "1"
 table["raw_category"]="Skin-care"
+table["report_funcuse"] = ""
 table["unit_type"] = 3
 table["component"]=""
-# %%
+
+table.to_csv("item_300972.csv", columns=["data_document_id","data_document_filename","prod_name","doc_date","rev_num","raw_category","raw_cas","raw_chem_name","report_funcuse","raw_min_comp","raw_max_comp","unit_type","ingredient_rank","raw_central_comp","component"], index=False)
