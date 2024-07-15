e7cf881f73a50b716d94032e40ec088c3531e02e
Michael Metcalf <92653661+mdmetcal@users.noreply.github.com>
2024-07-15 14:33:50 -0400

    Create BP_Air_Composition_Cleaned.py

---
+++ BP Air (2024)/BP_Air_Composition_Cleaned.py
@@ -0,0 +1,79 @@
+import os
+import pandas as pd
+import numpy as np
+import math
+
+pd.options.mode.chained_assignment = None
+os.chdir(r'C:\Users\mmetcalf\Documents and Scripts\BP Air')
+df = pd.read_csv('Factotum_BP_Air_(2024)_raw_extracted_records_20240715.csv')
+
+for index, row in df.iterrows():
+    #row['raw_max_comp'] = str(row['raw_max_comp']).replace('<','')
+    if 'NE' in str(row['raw_central_comp']):
+        df.drop(index, inplace=True)
+    elif '(Proprietar' in str(row['raw_central_comp']):
+        df.drop(index, inplace=True)
+    elif str(row['raw_min_comp']).isnumeric() and str(row['raw_max_comp']).isnumeric():
+        x = row['raw_min_comp']
+        y = row['raw_max_comp']
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif ('-' in str(row['raw_central_comp'])) and ('<' in str(row['raw_central_comp'])):
+        print(row['raw_central_comp'])
+        x = 0
+        y = 5
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '-' in str(row['raw_central_comp']):
+        split = str(row['raw_central_comp']).split('-')
+        x = float(split[0].strip())
+        y = float(split[1].strip())
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '<' in str(row['raw_central_comp']):
+        x = float(str(row['raw_central_comp']).replace('<', '').strip())
+        y = 0
+        df.at[index, 'lower_wf_analysis'] = y / 100
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '>' in str(row['raw_central_comp']):
+        y = float(str(row['raw_central_comp']).replace('>', '').strip())
+        x = 100
+        df.at[index, 'lower_wf_analysis'] = y / 100
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '<' in str(row['raw_max_comp']):
+        x = float(str(row['raw_max_comp']).replace('<', '').strip())
+        y = float(row['raw_min_comp'])
+        df.at[index, 'lower_wf_analysis'] = y / 100
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif '<' in str(row['raw_central_comp']):
+        x = float(str(row['raw_central_comp']).replace('<', '').strip())
+        df.at[index, 'lower_wf_analysis'] = 0
+        df.at[index, 'upper_wf_analysis'] = x / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif not math.isnan(float(row['raw_min_comp'])) and not math.isnan(float(row['raw_max_comp'])):
+        x = float(row['raw_min_comp'])
+        y = float(row['raw_max_comp'])
+        df.at[index, 'lower_wf_analysis'] = x / 100
+        df.at[index, 'upper_wf_analysis'] = y / 100
+        df.at[index, 'central_wf_analysis'] = ''
+    elif not math.isnan(float(row['raw_central_comp'])):
+        x = float(row['raw_central_comp'])
+        x = round(x / 100, 15)
+        df.at[index, 'lower_wf_analysis'] = ''
+        df.at[index, 'upper_wf_analysis'] = ''
+        df.at[index, 'central_wf_analysis'] = x
+    elif math.isnan(row['raw_min_comp']) and math.isnan(float(row['raw_max_comp'])) and math.isnan(float(row['raw_central_comp'])):
+        df.drop(index, inplace=True)
+
+df = df[['ExtractedComposition_id', 'lower_wf_analysis', 'central_wf_analysis', 'upper_wf_analysis']]
+
+df = df.fillna('')
+
+df.to_csv('BP_Air_Cleaned.csv', index=False)
+
