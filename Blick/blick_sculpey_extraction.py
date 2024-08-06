ab2f211f57bb0ed21f1f39a90e8515569d2f5bcf
larger-allison <45430904+larger-allison@users.noreply.github.com>
2024-08-06 11:50:40 -0400

    Add files via upload

---
+++ Blick/blick_sculpey_extraction.py
@@ -0,0 +1,195 @@
+import os, string, csv, re
+import pandas as pd
+from glob import glob
+
+
+def pdfToText(files):
+    """
+    Converts pdf files into text files
+    files: list of filenames
+    """
+    execfile = "pdftotext.exe"
+    execpath = 'C:\\Users\\alarger\\xpdf-tools-win-4.00\\bin64\\' #Path to execfile
+    for file in files:
+        pdf = '"'+file+'"'
+        cmd = os.path.join(execpath,execfile)
+        cmd = " ".join([cmd,"-nopgbrk","-table", "-enc UTF-8",pdf])
+        os.system(cmd)
+        
+    return
+
+
+def cleanLine(line):
+    """
+    Takes in a line of text and cleans it
+    removes non-ascii characters and excess spaces, and makes all characters lowercase
+    
+    """
+    clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty))
+    line=line.replace('é','e')
+    cline = clean(line.replace('–','-').replace('－','-'))
+    cline = cline.replace('–','-').replace('－','-')
+    cline = cline.lower()
+    cline = re.sub(' +', ' ', cline)
+    cline = cline.replace('\nspace\n','\n')
+    cline = cline.strip()
+    
+    return(cline)
+
+
+os.chdir(r'C:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Blick') #Folder pdfs are in
+pdfs = glob("*.pdf")
+txts = glob("*.txt")
+
+unconverted = []
+for p in pdfs: 
+    if p.replace('.pdf','.txt') not in txts:
+        unconverted.append(p)
+pdfToText(unconverted)
+
+fileList = glob("*.txt")       
+    
+findcas = lambda text: re.findall("[1-9][0-9]{1,6}\-[0-9]{2}\-[0-9]",text)
+    
+    
+idList = [] #list of product IDs
+filenameList = [] #list of file names matching those in the extacted text template
+prodnameList = [] #list of product names
+dateList = [] #list of msdsDates
+revList = [] #list of revision numbers
+catList = [] #list of product categories
+casList = [] #list of CAS numbers
+chemList = [] #list of chemical names
+useList = [] #list of functional uses of each chemical
+minList = [] #list of minimum concentrations
+maxList = [] #list of maximum concentrations
+unitList = [] #list of unit types (1=weight frac, 2=unknown, 3=weight percent,...)
+rankList = [] #list of ingredient ranks
+centList = [] #list of central concentrations
+componentList = [] #List of components
+
+i=0
+template = csv.reader(open('C:/Users/alarger/OneDrive - Environmental Protection Agency (EPA)/Profile/Documents/Blick/Factotum_Blick_(M)SDS_documents_20240705.csv')) 
+for row in template:
+    if row[7].split('_')[0].lower() == 'sculpey':
+        filename=row[6]
+        ID = row[0]
+    else: continue
+    
+
+    file=filename.replace('.pdf','.txt')    
+    ifile = open(file, encoding = 'utf8')
+    text = ifile.read()
+    cleaned = cleanLine(text)
+    cleaned = re.sub(' +', ' ', cleaned)
+    cleaned=cleaned.replace('\n ','\n')
+    if cleaned == '':
+        print('blank: ',file)
+   
+  
+    prodname = ''
+    date = ''
+    rev = ''
+    cat = ''
+    use = ''
+    component = ''
+    chem = []
+    cas = []
+    minC = []
+    maxC = []
+    centC = []
+    unit = []
+    
+    #Product name
+    if 'product trade name' in cleaned: 
+        prodname = cleaned.split('product trade name')[1]
+    prodname = prodname.split('product use')[0].split('substance name')[0].split('product code')[0]
+    prodname = prodname.replace('\n',' ').strip(':. ')
+    prodname = re.sub(' +', ' ', prodname)
+    
+    #Date
+    if 'issue date' in cleaned:
+        date = cleaned.split('issue date')[1]
+    date=date.split('\n')[0].strip('# .:')
+
+    
+    #Revision number
+    if 'version#' in cleaned:
+        rev = cleaned.split('version#')[1]
+    rev=rev.split('issue')[0].split('\n')[0].strip('# .:')
+    
+    #Raw category
+    if 'product use' in cleaned: 
+        cat = cleaned.split('product use')[1]
+    cat=cat.split('restrictions on use')[0].replace('\n',' ').strip(': .')
+    cat = re.sub(' +', ' ', cat)
+    
+    
+    #Ingredient section:
+    sec3=''
+    if 'components are proprietary, non-hazardous and/or present at amounts below reportable limits' in cleaned: 
+        #No ingredients
+        pass
+    else:
+        sec3=cleaned.split('section 3:')[1].split('section 4:')[0]
+        lines = sec3.split('\n')
+        lines = list(filter(None,lines))
+        for line in lines:
+            print(ID, line)
+            casnums = findcas(line)
+            if len(casnums) == 1:
+                chem.append(line.split(casnums[0])[0].strip())
+                cas.append(casnums[0])
+                centC.append(line.split(' ')[-1])
+            
+        
+    #clean up concentrations and names    
+    for c in range(0,len(chem)): 
+        chem[c] = re.sub(' +', ' ', chem[c])
+        minC.append('')
+        maxC.append('')
+        centC[c]=centC[c].replace('%','').replace('/','').replace('*','').strip()
+        if centC[c] != '':
+            unit.append('3')
+        else:
+            unit.append('')
+        if '-' in centC[c]:
+            minC[c] = centC[c].split('-')[0].strip('> ')
+            maxC[c] = centC[c].split('-')[1].strip(' <')
+            centC[c] = ''
+ 
+    #If no chems, leave fields blank
+    if chem == []:
+        chem = ['']
+        cas = ['']
+        minC = ['']
+        maxC = ['']
+        centC = ['']
+        unit = ['']
+        
+    n = len(chem)
+    idList.extend([ID]*n)
+    filenameList.extend([file.replace('.txt','.pdf')]*n)
+    prodnameList.extend([prodname]*n)
+    dateList.extend([date]*n)
+    revList.extend([rev]*n)
+    catList.extend([cat]*n)
+    casList.extend(cas)
+    chemList.extend(chem)
+    useList.extend([use]*n)
+    minList.extend(minC)
+    maxList.extend(maxC)
+    unitList.extend(unit)
+    centList.extend(centC)
+    componentList.extend([component]*n)
+    
+    if chem == ['']:
+        rankList.extend([''])
+    else:
+        rankList.extend(list(range(1,n+1)))
+   
+df = pd.DataFrame({'data_document_id':idList, 'data_document_filename':filenameList, 'prod_name':prodnameList, 'doc_date':dateList, 'rev_num':revList, 'raw_category':catList, 'raw_cas':casList, 'raw_chem_name':chemList, 'report_funcuse':useList, 'raw_min_comp': minList, 'raw_max_comp':maxList, 'unit_type':unitList, 'ingredient_rank':rankList, 'raw_central_comp':centList, 'component':componentList})
+
+df.to_csv(r'C:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\blick uploads\Sculpey Extracted Text.csv',index=False, header=True)
+
+  
