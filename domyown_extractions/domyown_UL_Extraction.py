7d806e1011f09ffd56c6e88511441ae12630bd97
larger-allison <45430904+larger-allison@users.noreply.github.com>
2026-01-12 09:42:38 -0500

    Add files via upload

---
+++ domyown_extractions/domyown_UL_Extraction.py
@@ -0,0 +1,273 @@
+import os, string, csv, re
+import pandas as pd
+from glob import glob
+
+
+def pdfToText(files):
+    """
+    Converts pdf files into text files
+    files: list of filenames
+    """
+    execfile = "pdftotext.exe"
+    execpath = 'C:\\Users\\alarger\\xpdf-tools-win-4.00\\bin64\\' #Path to execfile
+    for file in files:
+        pdf = '"'+file+'"'
+        cmd = os.path.join(execpath,execfile)
+        cmd = " ".join([cmd,"-nopgbrk","-table", "-enc UTF-8",pdf])
+        os.system(cmd)
+        
+    return
+
+
+def cleanLine(line):
+    """
+    Takes in a line of text and cleans it
+    removes non-ascii characters and excess spaces, and makes all characters lowercase
+    
+    """
+    clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty))
+    line=line.replace('é','e').replace('É','e').replace('²','').replace('≤','<=').replace('≥','>=')
+    cline = (line.replace('–','-').replace('－','-'))
+    cline = cline.lower()
+    cline = re.sub(' +', ' ', cline)
+    cline = cline.replace('\nspace\n','\n')
+    cline = cline.strip()
+    
+    return(cline)
+
+def ocrPdf(files):
+    """
+    Converts files into jpegs, uses pytesseract to OCR the jpegs, and writes the text into a txt file
+    files: list of filenames
+    """
+        
+  
+    
+    config = r'--psm 4 -c preserve_interword_spaces=1'
+    pytesseract.pytesseract.tesseract_cmd = r'c:\Users\alarger\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+    for p in files:
+        num = p.split('.pdf')[0]
+        pages = convert_from_path(p,500, poppler_path = r"C:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\poppler-0.68.0_x86\bin")
+        image_counter = 1
+        
+        #Make jpeg of each page
+        jpegs = []
+        for page in pages: 
+            filename = num+"page"+str(image_counter)+"_ocr.jpg"
+            page.save(filename, 'JPEG') 
+            image_counter = image_counter + 1
+            jpegs.append(filename)
+        filelimit = image_counter-1
+        
+        #Make text file
+        outfile = num+'_ocr.txt'
+        f = open(outfile, "a") 
+        
+        #Perform OCR on jpegs and write to text file 
+        for i in range(1, filelimit + 1): 
+            # print(i)
+            filename = num+"page"+str(i)+"_ocr.jpg"
+            text = str(((pytesseract.image_to_string(Image.open(filename), config = config))))    
+            # print(text)
+            text=clean(text)
+            f.write(text) 
+            f.write('\n')
+        
+        f.close() 
+
+
+os.chdir(r'C:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\domyown\Factotum_DoMyOwn_SDS_2024_documents_20251215') #Folder pdfs are in
+pdfs = glob("*.pdf")
+txts = glob("*.txt")
+
+unconverted = []
+for p in pdfs: 
+    if p.replace('.pdf','.txt') not in txts:
+        unconverted.append(p)
+pdfToText(unconverted)
+
+fileList = glob("*.txt")       
+clean = lambda dirty: ''.join(filter(string.printable.__contains__, dirty))
+findcas = lambda text: re.findall("[1-9][0-9]{1,6}\-[0-9]{2}\-[0-9]",text) #finds cas number     
+findecs = lambda text: re.findall("[0-9]{3}\-[0-9]{3}\-[0-9]",text) #finds ec number
+findreachs = lambda text: re.findall("[0-9]{2}\-[0-9]{10}\-[0-9]{2}\-",text) #finds reach number
+    
+    
+idList = [] #list of product IDs
+filenameList = [] #list of file names matching those in the extacted text template
+prodnameList = [] #list of product names
+dateList = [] #list of msdsDates
+revList = [] #list of revision numbers
+catList = [] #list of product categories
+casList = [] #list of CAS numbers
+chemList = [] #list of chemical names
+useList = [] #list of functional uses of each chemical
+minList = [] #list of minimum concentrations
+maxList = [] #list of maximum concentrations
+unitList = [] #list of unit types (1=weight frac, 2=unknown, 3=weight percent,...)
+rankList = [] #list of ingredient ranks
+centList = [] #list of central concentrations
+componentList = [] #List of components
+
+i=0
+template = csv.reader(open(r'c:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\domyown\Factotum_DoMyOwn_SDS_(2024)_unextracted_documents_20260107.csv')) 
+for row in template:
+    filename=row[1]
+    ID = row[0]
+    
+    
+    if filename not in pdfs: continue
+    file=filename.replace('.pdf','')+'.txt'    
+    try: ifile = open(file, encoding = 'utf8')
+    except: 
+        if os.path.isfile((file.replace('.txt','_ocr.txt'))) == False:
+            ocrPdf([filename])
+        
+        ifile = open((file.replace('.txt','_ocr.txt')), encoding = 'utf8')
+        text = ifile.read()
+        cleaned = cleanLine(text)
+        cleaned = re.sub(' +', ' ', cleaned)
+        cleaned=cleaned.replace('\n ','\n')
+        # continue
+    text = ifile.read()
+    cleaned = cleanLine(text)
+    cleaned = re.sub(' +', ' ', cleaned)
+    cleaned=cleaned.replace('\n ','\n')
+    if cleaned == '':
+
+        if os.path.isfile((file.replace('.txt','_ocr.txt'))) == False:
+            ocrPdf([filename])
+        
+        ifile = open((file.replace('.txt','_ocr.txt')), encoding = 'utf8')
+        text = ifile.read()
+        cleaned = cleanLine(text)
+        cleaned = re.sub(' +', ' ', cleaned)
+        cleaned=cleaned.replace('\n ','\n')
+
+
+   
+    if 'ul sds template' not in cleaned: continue
+  
+    prodname = ''
+    date = ''
+    rev = ''
+    cat = ''
+    use = ''
+    component = ''
+    chem = []
+    cas = []
+    minC = []
+    maxC = []
+    centC = []
+    unit = []
+    
+    
+    
+
+    #Product name
+    if 'product name' in cleaned: 
+        prodname = cleaned.split('product name')[1]
+    prodname = prodname.split('other means of identification')[0].split('product code')[0]
+    prodname = clean(prodname).replace('\n',' ').strip(':. ')
+    prodname = re.sub(' +', ' ', prodname)
+    
+    #Date and version
+    if 'revision number' in cleaned:
+        rev = cleaned.split('revision number')[1].split('\n')[0].strip(' -:')
+   
+    if 'revision date' in cleaned:
+        date = cleaned.split('revision date')[1].split('revision')[0].split('\n')[0].strip(' -:')
+
+
+    #Raw category
+    if 'recommended use of the chemical and restrictions on use\nrecommended use' in cleaned: 
+        cat = cleaned.split('recommended use of the chemical and restrictions on use\nrecommended use')[1]
+    cat=cat.split('restrictions')[0].split('details of the supplier')[0].split('uses advised against')[0].replace('\n',' ').strip(': .')
+    cat = re.sub(' +', ' ', cat)
+    
+    
+    #Ingredient section:
+    sec3=''
+    ec=0
+    betweenPages=False
+    inchem=False
+    if '3. composition/information on ingredients' in cleaned: 
+        sec3=cleaned.split('3. composition/information on ingredients')[1].split('4. first')[0].split('the exact percentage')[0]
+        lines = sec3.split('\n')
+        lines = list(filter(None,lines))
+        for line in lines:
+            line = line.strip()
+            if betweenPages == True:
+                if 'revision date' in line:
+                    betweenPages=False
+                continue
+            if 'page' in line or 'item numbers' in line:
+                betweenPages=True
+                continue
+            if 'chemical name cas no' in line: continue
+            if line.strip(': ,.-*') in ['substance','not applicable','mixture','information review act','registry number (hmira','registry #)','']: continue            
+            
+            print(ID,line)
+            casrns = findcas(line)
+            if len(casrns)==0 and 'proprietary' in line: casrns.append('proprietary')
+            if len(casrns)== 1:
+                chem.append(line.split(casrns[0])[0].strip())
+                cas.append(casrns[0])
+                centC.append(line.split(casrns[0])[-1].strip(' -*'))
+            elif len(casrns) == 0 and len(chem)>0:
+                chem[-1] = (chem[-1] + ' ' + line).strip()
+            else: print('HALP',ID,line)
+            
+     
+    
+    #clean up concentrations and names    
+    for c in range(0,len(chem)): 
+        minC.append('')
+        maxC.append('')
+        chem[c] = re.sub(' +', ' ', chem[c])
+        centC[c]=centC[c].replace('%','').replace('/','').replace('*','').strip()
+        if centC[c] != '' or maxC[c]!='' or minC[c]!='':
+            unit.append('3')
+        else:
+            unit.append('')
+        if '-' in centC[c]:
+            minC[c] = centC[c].split('-')[0].strip('> ')
+            maxC[c] = centC[c].split('-')[1].strip(' <')
+            centC[c] = ''
+ 
+    #If no chems, leave fields blank
+    if chem == []:
+        chem = ['']
+        cas = ['']
+        minC = ['']
+        maxC = ['']
+        centC = ['']
+        unit = ['']
+        
+    n = len(chem)
+    idList.extend([ID]*n)
+    filenameList.extend([file.replace('.txt','.pdf')]*n)
+    prodnameList.extend([prodname]*n)
+    dateList.extend([date]*n)
+    revList.extend([rev]*n)
+    catList.extend([cat]*n)
+    casList.extend(cas)
+    chemList.extend(chem)
+    useList.extend([use]*n)
+    minList.extend(minC)
+    maxList.extend(maxC)
+    unitList.extend(unit)
+    centList.extend(centC)
+    componentList.extend([component]*n)
+    
+    if chem == ['']:
+        rankList.extend([''])
+    else:
+        rankList.extend(list(range(1,n+1)))
+   
+df = pd.DataFrame({'data_document_id':idList, 'data_document_filename':filenameList, 'prod_name':prodnameList, 'doc_date':dateList, 'rev_num':revList, 'raw_category':catList, 'raw_cas':casList, 'raw_chem_name':chemList, 'report_funcuse':useList, 'raw_min_comp': minList, 'raw_max_comp':maxList, 'unit_type':unitList, 'ingredient_rank':rankList, 'raw_central_comp':centList, 'component':componentList})
+
+df.to_csv(r'C:\Users\alarger\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\domyown\UL domyown Extracted Text.csv',index=False, header=True)
+
+  
